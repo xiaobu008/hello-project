f807990a9acc41050d6a43fcadb85d07497a3549
xiaobu <dp825496966@qq.com>
2019-02-21 13:58:53 +0800

    添加hello_project工程

---
+++ src/cn/itcast/git/Hello.java
@@ -0,0 +1,18 @@
+package cn.itcast.git;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello project...");
+        System.out.println("hello project...");
+        System.out.println("hello project...");
+        System.out.println("hello project...");
+        System.out.println("hello project...");
+        System.out.println("hello project...");
+        System.out.println("hello project...");
+        System.out.println("hello project...");
+    }
+
+    public void test(){
+        System.out.println("11111111111");
+    }
+}
